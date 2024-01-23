f08dd6a4be44d17fdfb3a031603a463c3e334a85
ShauryaMalhan <seemashaurya13@gmail.com>
2024-01-24 01:11:10 +0530

    Added random intervals for spider

--- index.js
+++ index.js
@@ -12,22 +12,26 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-// app.get('/spider_web_game', (req, res) => {
-//     res.render('game');
-// });
-
-app.get('/start_game', (req, res) => {
-    
+app.get('/game', (req, res) => {
     res.render('game');
-    // Execute the Python script using child_process
-    exec('python3 main.py', { cwd: __dirname }, (error, stdout, stderr) => {
-        if(error) {
-            console.error(`Erorrr!!! ${error}`);
-        }else{
-            console.log("Yayyy it ran successfully");
-        }
-    });
 });
+
+// app.get('/start_game', (req, res) => {
+//     // Respond immediately with the 'game' view
+//     res.render('game');
+
+//     // Execute the Python script in the background
+//     exec('python3 main.py', { cwd: __dirname }, (error, stdout, stderr) => {
+//         if (error) {
+//             console.error(`Error!!! ${error.message}`);
+//         } else {
+//             console.log("Yayyy it ran successfully");
+//             // Optionally, you can log the stdout and stderr
+//             console.log(`stdout: ${stdout}`);
+//             console.error(`stderr: ${stderr}`);
+//         }
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
